be0ce819c93786a3af629774e6b0cfd753faa0ff
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-10-21 17:05:41 -0500

    backend done, i think

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,20 @@ router.get('/', (req, res) => res.send('This is api root!'))
 router.get('/main', controllers.getAll)
 
 router.get('/restaurants/:id', controllers.getRestById)
-
-// router.get('/restaurants', controllers.getAllRestaurantsAlpha)
-
-router.get('/restaurants', controllers.getAllRestaurantsCity)
-
+router.put('/restaurants/:id', controllers.getRestById)
+router.delete('/restaurants/:id', controllers.deleteRest)
+router.get('/restaurants/comments', controllers.getCommentsByRest)
 router.post('/newrestaurant', controllers.createRestaurant)
 
+router.get('/restaurants/alpha', controllers.getAllRestaurantsAlpha)
+router.get('/restaurants', controllers.getAllRestaurantsCity)
+
 router.get('/users', controllers.getAllUsers)
-
 router.get('/users/:id', controllers.getUserById)
+router.get('/users/one', controllers.getUser)
 
-router.get('/users', controllers.getUser)
-
-router.post('/postComment', controllers.makeComment)
+router.post('/comment', controllers.getCommentsByUser)
+router.delete('/comment', controllers.deleteComment)
+router.get('/comment', controllers.getCommentsByUser)
 
 module.exports = router
